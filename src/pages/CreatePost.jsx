d8bf75b9ea439c790195979dac92b6765b0cddd3
Soham Kulkarni <sohamkulkarni.insnapsys@gmail.com>
2025-10-29 17:24:00 +0530

    tinymce integration 5

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -91,9 +91,11 @@ export default function CreatePost() {
               skin: 'oxide-dark',
               content_css: "dark",
               plugins: [
-                'anchor autolink charmap codesample emoticons link lists media searchreplace table visualblocks wordcount',
-                'checklist mediaembed casechange formatpainter pageembed a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode advtemplate ai uploadcare mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown importword exportword exportpdf'
-              ],
+                    'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                    'checklist', 'mediaembed', 'casechange', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste',
+                    'advtable', 'advcode', 'advtemplate', 'ai', 'uploadcare', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect',
+                    'typography', 'inlinecss', 'markdown', 'importword', 'exportword', 'exportpdf'
+                ],
               toolbar:
                 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography uploadcare | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
               tinycomments_mode: 'embedded',
